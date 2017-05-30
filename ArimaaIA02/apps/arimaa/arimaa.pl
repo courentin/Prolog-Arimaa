9e02783d964daadf0ff21ce5311a70dbf179b663
Corentin Hembise <corentinhembise@free.fr>
2017-05-30 17:57:01 +0200

    is_ally & is_enemy

--- ArimaaIA02/apps/arimaa/arimaa.pl
+++ ArimaaIA02/apps/arimaa/arimaa.pl
@@ -20,6 +20,7 @@ get_moves([[[1,0],[4,0]],[[0,0],[1,0]],[[0,1],[0,0]],[[0,0],[0,1]]], Gamestate, 
 board([[0,0,rabbit,silver],[0,1,rabbit,silver],[0,2,horse,silver],[0,3,rabbit,silver],[0,4,elephant,silver],[0,5,rabbit,silver],[0,6,rabbit,silver],[0,7,rabbit,silver],[1,0,camel,silver],[1,1,cat,silver],[1,2,rabbit,silver],[1,3,dog,silver],[1,4,rabbit,silver],[1,5,horse,silver],[1,6,dog,silver],[1,7,cat,silver],[2,7,rabbit,gold],[6,0,cat,gold],[6,1,horse,gold],[6,2,camel,gold],[6,3,elephant,gold],[6,4,rabbit,gold],[6,5,dog,gold],[6,6,rabbit,gold],[7,0,rabbit,gold],[7,1,rabbit,gold],[7,2,rabbit,gold],[7,3,cat,gold],[7,4,dog,gold],[7,5,rabbit,gold],[7,6,horse,gold],[7,7,rabbit,gold]]).
 
 % Retourne la piece à la position [X, Y]
+% what_on([X, Y], Board, Résultat)
 what_on([_, _], [], []).
 what_on([X, Y], [[X, Y, Piece, Couleur]|_], [Piece, Couleur]) :- !.
 what_on([X, Y], [T|Q], Res) :- what_on([X, Y], Q, Res).
@@ -32,3 +33,8 @@ get_adjacentes([X, Y], [U, R, D, L], Board) :-
   YD is Y + 1, what_on([X, YD],Board,R),
   XR is X + 1, what_on([XR, Y],Board,D),
   YU is Y - 1, what_on([X, YU],Board,L).
+
+% is_enemy([X, Y], Gamestate, Board)
+is_ally([X, Y] , [Side|_], Board) :- what_on([X,Y], Board, [_,Side]).
+
+is_enemy([X, Y] , [Side|_], Board) :- what_on([X,Y], Board, [_,Color]), Side \= Color.
